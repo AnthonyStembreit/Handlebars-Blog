1db1c755b03555a60605506ae505de00472548ce
AnthonyStembreit <stembreit@gmail.com>
2021-10-15 19:58:24 -0500

    created main handlebar routes

--- controllers/html_routes/main_html.js
+++ controllers/html_routes/main_html.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../models');
 
-
-
-router.get("/", ()=>{
+router.get("/", async (req, res) =>{
 //view all posts 
+let posts = await db.Post.findAll()
+console.log(posts)
+res.render("all-posts", {posts: posts, dashboard: false})
 })
 
-router.get("/:postid",()=>{
+router.get("/:postid", async(req, res)=>{
 //view one post
+let post = await db.Post.findOne({
+    where:{
+        id: req.params.id
+    }
+})
+console.log(post)
+res.render("single-post", {post})
 })
 
 //trying to get modal to work but set up these in case
